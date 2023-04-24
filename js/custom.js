0067fd7893413f30a47bd3aa843480234b31e94a
41ahmad <111183178+41ahmad@users.noreply.github.com>
2023-04-24 18:57:26 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@ var center;
 function initialize() {
     var mapOptions = {
       zoom: 16,
-      center: new google.maps.LatLng(13.758468, 100.567481),
+      center: new google.maps.LatLng(11.9838698,8.5611171),
       scrollwheel: false
     };
   
